28ac8c24fe8c41ef5fb7b4557dbd8ec18c6765f8
lorancostellow <lorna.costelloe@mycit.ie>
2016-04-22 02:20:54 +0100

    Basic html and CSS
    Documentation section 1 - 6. 
    Basic site code that will be repeated throughout.

---
+++ spring-boot-basewebapp/src/main/resources/public/contact.php
@@ -0,0 +1,31 @@
+<!-- Author: Lorna Costelloe-->
+<!DOCTYPE html>
+<html>
+	<head> 
+		<meta charset="UTF-8" />
+	   <link rel="stylesheet" type="text/css" media="all" href="/css/style.css" />
+	    <title>Lockard Security</title>
+	</head>
+	<body>  
+		<header>
+		<div class ="logo">
+			<img src ="/LOGO_PLACEHOLDER.PNG" alt="Lockard Security Logo">
+			<img src ="/LOGO_PLACEHOLDER.PNG" alt="Lockard Security Logo">
+			<img src ="/LOGO_PLACEHOLDER.PNG" alt="Lockard Security Logo">
+			<img src ="/LOGO_PLACEHOLDER.PNG" alt="Lockard Security Logo">
+		</div>
+	    <div class="nav">
+	      <ul>
+	        <li class="home"><a href="/hello.php">Home</a></li>
+	        <li class="products"><a href="/products.php">Products</a></li>
+	        <li class="about"><a href="/about.php">About Us</a></li>
+	        <li class="contact"><a class="active" href="/contact.php">Contact Us</a></li>
+	        <li class="signin"><a href="/login.php">Log In</a></li>
+	      </ul>
+	    </div>
+	  </header>
+	  
+	      
+  </body>
+	<footer></footer>
+</html>
