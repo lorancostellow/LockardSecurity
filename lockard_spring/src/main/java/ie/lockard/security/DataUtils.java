6bd0ff41c607a71263e04d844b31358866c1078d
William Barrett <william.barrett@mycit.ie>
2016-04-26 23:52:29 +0100

    Added HAshing algorithms

--- lockard_spring/src/main/java/ie/lockard/security/DataUtils.java
+++ lockard_spring/src/main/java/ie/lockard/security/DataUtils.java
@@ -1,31 +1,73 @@
 package ie.lockard.security;
-
+import org.apache.commons.codec.binary.Base64;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.util.Base64;
-import java.util.Random;
-
+import java.security.spec.InvalidKeySpecException;
 /**
- * Created by dylan on 24/04/16.
+ * Created by William on 24/04/16.
  * Source belongs to Lockard
  */
 public class DataUtils {
 
+    private static final int iterations = 20*1000;
+    private static final int saltLen = 32;
+    private static final int desiredKeyLen = 256;
+
 
     public static boolean checkHash(String hash, String password) {
-        //TODO Implement @William
-        return true;
+        String[] saltAndPass = hash.split("\\$");
+        if (saltAndPass.length != 2) {
+            throw new IllegalStateException(
+                    "The stored password have the form 'salt$hash'");
+        }
+        String hashOfInput = hash(password, Base64.decodeBase64(saltAndPass[0]));
+        return hashOfInput.equals(saltAndPass[1]);
     }
 
     public static String getHash(String password) {
-        //TODO Implement @William
-        return password + " - hash";
+        byte[] salt = new byte[0];
+        try {
+            salt = SecureRandom.getInstance("SHA1PRNG").generateSeed(saltLen);
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("no");
+        }
+        // store the salt with the password
+        return Base64.encodeBase64String(salt) + "$" + hash(password, salt);
+    }
+
+    private static String hash(String password, byte[] salt) {
+        if (password == null || password.length() == 0)
+            throw new IllegalArgumentException("Empty passwords are not supported.");
+
+        SecretKey key = null;
+        try {
+            SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            key = f.generateSecret(new PBEKeySpec(
+                            password.toCharArray(), salt, iterations, desiredKeyLen)
+            );
+        } catch (InvalidKeySpecException e) {
+            System.out.println("no");
+        } catch (NoSuchAlgorithmException e){
+            System.out.println("no");
+        }
+        return Base64.encodeBase64String(key.getEncoded());
     }
 
     public static String getToken() {
         byte[] byteToken = new byte[16];
         new SecureRandom().nextBytes(byteToken);
-        return Base64.getEncoder()
+        return java.util.Base64.getEncoder()
                 .encodeToString(byteToken)
-                .substring(0,16);
+                .substring(0, 16);
     }
+
+//    public static void main(String[] args){
+//        if(checkHash(getHash("bannanna"),"bannanna"))
+//           System.out.println("yes");
+//        else
+//            System.out.println("no");
+//      }
 }
